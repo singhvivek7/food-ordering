d698295016f18ba07d752a45db1872faa83e5c68
Vivek <vivek.k@paybolt.in>
2025-03-04 14:17:07 +0530

    done: icons

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,77 +1,64 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { FontAwesome6 } from '@expo/vector-icons';
 
-import { HelloWave } from '@/src/components/HelloWave';
-import ParallaxScrollView from '@/src/components/ParallaxScrollView';
-import { ThemedText } from '@/src/components/ThemedText';
-import { ThemedView } from '@/src/components/ThemedView';
+import profileImg from '@/assets/images/profile.avif';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{' '}
-          to see changes. Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{' '}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{' '}
-          directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: 'https://images.pexels.com/photos/268941/pexels-photo-268941.jpeg',
+        }}
+        style={styles.backgroundImage}
+      />
+      <Image source={profileImg} style={styles.photo} />
+      <Text style={styles.name}>Vivek Singh</Text>
+      <Text style={styles.description}>Web Developer</Text>
+
+      <View style={styles.socialContainer}>
+        <FontAwesome6 name="github" size={16} color="black" />
+        <FontAwesome6 name="linkedin" size={16} color="black" />
+        <FontAwesome6 name="x-twitter" size={16} color="black" />
+        <FontAwesome6 name="instagram" size={16} color="black" />
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    gap: 5,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  name: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#353636',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  description: {
+    fontSize: 16,
+  },
+  photo: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginTop: -100,
+    borderWidth: 5,
+    borderColor: '#fff',
+  },
+  backgroundImage: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+  },
+  socialContainer: {
+    flexDirection: 'row',
+    gap: 8,
+    marginVertical: 8,
   },
 });
