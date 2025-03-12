64e34d9b56b5869e8491efc2100c3d0d5760d514
Vivek <vivek.k@paybolt.in>
2025-03-12 12:24:04 +0530

    feat: add UI - fitness

---
+++ section_05_fitness/src/app/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'expo-router';
+import { View, Text } from '@/components/general/Themed';
+import CustomButton from '@/components/general/CustomButton';
+import WorkoutListItem from '@/components/workouts/WorkoutListItem';
+import workouts from '@/data/dummyWorkouts';
+import { FlatList } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 10,
+        padding: 10,
+        backgroundColor: 'transparent',
+      }}
+    >
+      <Link href="/workout/current" asChild>
+        <CustomButton title="Resume workout" />
+      </Link>
+
+      <FlatList
+        data={workouts}
+        contentContainerStyle={{ gap: 8 }}
+        renderItem={({ item }) => <WorkoutListItem workout={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+}
